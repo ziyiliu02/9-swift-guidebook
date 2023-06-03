d9cf8cee839425b0a7a71ddd487241204dab8796
Liu Ziyi <ziyi.liu02@gmail.com>
2023-06-03 21:10:02 +0800

    Initial Commit

---
+++ Shared/__swift_guidebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  __swift_guidebookApp.swift
+//  Shared
+//
+//  Created by Liu Ziyi on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct __swift_guidebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
